8e86c02bc691c88af7376c9a9c4eeb6b15d98375
DZLZH <dzlzh@null.net>
2016-06-07 10:18:35 +0200

    PDO insert,update

--- PDOMysql.class.php
+++ PDOMysql.class.php
@@ -27,7 +27,7 @@ class PDOMysql
     {
         $dsn = $dbconfig['type'] . ':host=' . $dbconfig['host'] . ';port=' . $dbconfig['port'] . ';dbname=' . $dbconfig['dbname'];
         try {
-            $this->con = new PDO($dsn, $dbconfig['dbuser'], $dbconfig['dbpwd']);
+            $this->con = new PDO($dsn, $dbconfig['dbuser'], $dbconfig['dbpwd'], array(PDO::ATTR_PERSISTENT => true));
             $setcharset = 'set names ' . $dbconfig['dbcharset'];
             $this->con->exec($setcharset);
             return true;
@@ -83,10 +83,9 @@ class PDOMysql
      *
      * @return void
      */
-    public function insert($table, $param, $insertID = true)
+    public function insert($table, $param, $insertID = false)
     {
         $keys = array_keys($param);
-        var_dump($keys);
         $fields = $keys;
         array_walk($fields, array('PDOMysql', 'addSpecialChar'));
         $fields = implode(',', $fields);
@@ -96,9 +95,36 @@ class PDOMysql
         }
         $parameters = implode(',', $parameters);
         $sql = 'INSERT INTO `' . $table . '` (' . $fields . ') VALUE (' . $parameters . ')';
-        echo $sql;
-        var_dump($fields);
-        var_dump($parameters);
+        $stmt = $this->query($sql, $param);
+        if ($insertID) {
+            return $this->con->lastInsertId();
+        } else {
+            return self::errorMsg($stmt);
+        }
+        
+    }
+
+    /**
+     * update
+     *
+     * @return void
+     */
+    public function update($table, $param, $where = null)
+    {
+        $keys = array_keys($param);
+        $fields = $keys;
+        array_walk($fields, array('PDOMysql', 'addSpecialChar'));
+        foreach ($keys as $key => $value) {
+            $parameters[$key] = $fields[$key] . '=:' . $value;
+        }
+        $parameters = implode(',', $parameters);
+        $sql = 'UPDATE `' . $table . '` SET ' . $parameters;
+        if (!empty($where)) {
+            $sql .= ' WHERE ' . $where;
+        }
+        $stmt = $this->query($sql, $param);
+        return self::errorMsg($stmt);
+        
     }
     
     /**
@@ -113,6 +139,24 @@ class PDOMysql
         }
         return $param;
     }
+
+    /**
+     * errorMsg
+     *
+     * @return void
+     */
+    private function errorMsg($stmt)
+    {
+        $errorCode = $stmt->errorCode();
+        if ($errorCode !== '00000') {
+            $errorInfo = $stmt->errorInfo();
+            $errorMsg = 'ERROR ' . $errorInfo[1] . ' (' . $errorInfo[0] . '):' . $errorInfo[2];
+            return $errorMsg;
+        } else {
+            return $stmt->rowCount();
+        }
+    }
+    
     
     
     
@@ -121,20 +165,32 @@ class PDOMysql
 
 
 require_once 'config.php';
+date_default_timezone_set('Asia/Shanghai');
 $pdo = new PDOMysql();
 $pdo->connect($dbconfig);
 $param = array(
-    // '.' => '',
-    // '*' => '',
-    // '``' => '',
-    'type'      => 'mysql',
-    'host'      => '',
-    'port'      => '3306',
-    'dbname'    => '',
-    'dbuser'    => '',
-    'dbpwd'     => '',
-    'dbcharset' => 'utf8',
-    'dbdebug'   => true,
+    // 'uuid'              => 'UUID',
+    'positionId'        => '111',
+    'positionName'      => '1职位名称',
+    'positionType'      => '1职位类型',
+    'positionAdvantage' => '1职位诱惑',
+    'companyName'       => '1公司名称',
+    'companyShortName'  => '1公司简称',
+    'companySize'       => '1公司规模',
+    'companyHome'       => '1公司主页',
+    'industryField'     => '1行业领域',
+    'financeStage'      => '1融资阶段',
+    'city'              => '1城市',
+    'district'          => '1区域',
+    'businessZone'      => '1商业区',
+    'address'           => '1具体地址',
+    'salary'            => '1薪水',
+    'workYear'          => '1工作经验',
+    'education'         => '1学历要求',
+    'jobNature'         => '1工作性质',
+    'jobDescription'    => '1职位描述',
+    'createTime'        => date("Y-m-d H:i:s"),
+    'collectionTime'    => date("Y-m-d H:i:s"),
 );   
 // var_dump($pdo->findOne('select UUID();'));
-$pdo->insert('1',$param);
+var_dump($pdo->update('lagou',$param, 'uuid="UUID"'));
