fa946301d2cf78427ec16ec1b0b4a63ee08da85d
DZLZH <dzlzh@null.net>
2016-06-13 05:01:54 +0200

    select positionIds

--- crawler.php
+++ crawler.php
@@ -25,45 +25,56 @@ foreach ($parameters as $key => $value) {
     }
 }
 
-for ($i = 1; $i < 10; $i++) {
+$positionIds = $pdo->findAll('SELECT `positionId` FROM lagou;');
+foreach ($positionIds as $key => $value) {
+    $positionIds[$key] = $value['positionId'];
+}
+
+for ($i = 1; $i <= 2; $i++) {
     $jobsUrl = $jobsBaseUrl . 'pn=' . $i;
+    echo $jobsUrl . "\n";
     $jsonData = json_decode(curlHtml($jobsUrl), true);
     $jobs = $jsonData['content']['positionResult']['result'];
     foreach ($jobs as $job) {
-        $jobUrl = $jobBaseUrl . $job['positionId'] . '.html';
-        $jobData = curlHtml($jobUrl);
-        $isMatched = preg_match('/var\spositionAddress\s=\s\'([^\']+)\'/', $jobData, $matches);
-        $address = $isMatched ? $matches[1] : null;
-        $isMatched = preg_match('/<dd\sclass="job_bt">\s*<h3[^>]*>.*<\/h3>((?:.|\n)*?)<\/dd>/', $jobData, $matches);
-        $jobDescription = $isMatched ? preg_replace("/(\s|\&nbsp\;|　|\xc2\xa0)/", "", strip_tags($matches[1])) : null;
-        $param = array(
-            'uuid'              => getUUID(),
-            'positionId'        => $job['positionId'],
-            'positionName'      => $job['positionName'],
-            'positionType'      => $job['positionType'],
-            'positionAdvantage' => $job['positionAdvantage'],
-            'companyName'       => $job['companyName'],
-            'companyShortName'  => $job['companyShortName'],
-            'companySize'       => $job['companySize'],
-            'companyLabelList'  => empty($job['companyLabelList']) ? null : implode(',', $job['companyLabelList']),
-            'industryField'     => $job['industryField'],
-            'financeStage'      => $job['financeStage'],
-            'city'              => $job['city'],
-            'district'          => $job['district'],
-            'businessZones'     => empty($job['businessZones']) ? null : implode(',', $job['businessZones']),
-            'address'           => $address,
-            'salary'            => $job['salary'],
-            'workYear'          => $job['workYear'],
-            'education'         => $job['education'],
-            'jobNature'         => $job['jobNature'],
-            'jobDescription'    => $jobDescription,
-            'createTime'        => $job['createTime'],
-            'jobUrl'            => $jobUrl,
-            'collectionTime'    => date("Y-m-d H:i:s"),
-        );   
-        var_dump($pdo->insert('lagou',$param));
+        if (in_array($job['positionId'], $positionIds)) {
+            continue;
+        } else {
+            $jobUrl = $jobBaseUrl . $job['positionId'] . '.html';
+            echo $jobUrl . "\n";
+            $jobData = curlHtml($jobUrl);
+            $isMatched = preg_match('/var\spositionAddress\s=\s\'([^\']+)\'/', $jobData, $matches);
+            $address = $isMatched ? $matches[1] : null;
+            $isMatched = preg_match('/<dd\sclass="job_bt">\s*<h3[^>]*>.*<\/h3>((?:.|\n)*?)<\/dd>/', $jobData, $matches);
+            $jobDescription = $isMatched ? preg_replace("/(\s|\&nbsp\;|　|\xc2\xa0)/", "", strip_tags($matches[1])) : null;
+            $param = array(
+                'uuid'              => getUUID(),
+                'positionId'        => $job['positionId'],
+                'positionName'      => $job['positionName'],
+                'positionType'      => $job['positionType'],
+                'positionAdvantage' => $job['positionAdvantage'],
+                'companyName'       => $job['companyName'],
+                'companyShortName'  => $job['companyShortName'],
+                'companySize'       => $job['companySize'],
+                'companyLabelList'  => empty($job['companyLabelList']) ? null : implode(',', $job['companyLabelList']),
+                'industryField'     => $job['industryField'],
+                'financeStage'      => $job['financeStage'],
+                'city'              => $job['city'],
+                'district'          => $job['district'],
+                'businessZones'     => empty($job['businessZones']) ? null : implode(',', $job['businessZones']),
+                'address'           => $address,
+                'salary'            => $job['salary'],
+                'workYear'          => $job['workYear'],
+                'education'         => $job['education'],
+                'jobNature'         => $job['jobNature'],
+                'jobDescription'    => $jobDescription,
+                'createTime'        => $job['createTime'],
+                'jobUrl'            => $jobUrl,
+                'collectionTime'    => date("Y-m-d H:i:s"),
+            );
+            var_dump($pdo->insert('lagou',$param));
+        }
     }
-        die;
+        echo "----------------------------\n";
 }
 
 // var_dump($pdo->findOne('select UUID();'));
