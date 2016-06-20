81972ab6c70d9ce6838c0c21409554867bbf27c6
DZLZH <dzlzh@null.net>
2016-06-20 10:37:43 +0200

    districts

--- analysis.php
+++ analysis.php
@@ -22,3 +22,11 @@ $pdo->connect($dbconfig);
 $totalSql = 'SELECT count(uuid) AS number FROM lagou';
 $total = $pdo->findOne($totalSql)['number'];
 echo 'Total:', $total, "\n";
+echo "-------------------------\n";
+
+$districtSql = 'SELECT district, COUNT(district) AS number FROM lagou GROUP BY district ORDER BY number DESC';
+$districts = $pdo->findAll($districtSql);
+foreach ($districts as $key=>$value) {
+    echo $value['district'], ":", $value['number'], "\n";
+}
+
