95bedd599388652276f538d25b05a46be916bf72
DZLZH <dzlzh@null.net>
2016-06-20 10:49:23 +0200

    financeStage,proportion

--- analysis.php
+++ analysis.php
@@ -26,7 +26,16 @@ echo "-------------------------\n";
 
 $districtSql = 'SELECT district, COUNT(district) AS number FROM lagou GROUP BY district ORDER BY number DESC';
 $districts = $pdo->findAll($districtSql);
-foreach ($districts as $key=>$value) {
-    echo $value['district'], ":", $value['number'], "\n";
+foreach ($districts as $value) {
+    $proportion = round(($value['number']/$total)*100, 2) . '%';
+    echo $value['district'], ':', $value['number'], " —— ", $proportion, "\n";
 }
+echo "-------------------------\n";
 
+$financeStageSql = 'SELECT financeStage, COUNT(financeStage) AS number FROM lagou GROUP BY financeStage ORDER BY number DESC';
+$financeStages = $pdo->findAll($financeStageSql);
+foreach ($financeStages as $value) {
+    $proportion = round(($value['number']/$total)*100, 2) . '%';
+    echo $value['financeStage'], ':', $value['number'], " —— ", $proportion, "\n";
+}
+echo "-------------------------\n";
