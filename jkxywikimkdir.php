396d18e13af7c9d557d1ebcb9886443d3e2f58c3
DZLZH <dzlzh@null.net>
2016-07-05 09:13:57 +0200

    Refactoring jkxywikimkdir

--- jkxywikimkdir.php
+++ jkxywikimkdir.php
@@ -14,40 +14,10 @@
  */
 
 require_once 'config.php';
-$rootPath = $directory . DIRECTORY_SEPARATOR . $rootDirectoryName;
-// print_r($jkxyWikiUrlList);
-if (makeDirectory($rootPath) != 1) {
-    foreach ($jkxyWikiUrlList as $key => $value) {
-        $categoryPath = $rootPath . DIRECTORY_SEPARATOR . $key;
-        if (makeDirectory($categoryPath) != 1) {
-            foreach ($value as $k => $v) {
-                $listPath = $categoryPath . DIRECTORY_SEPARATOR . $k;
-                switch (makeDirectory($listPath)) {
-                    case 0:
-                        echo $listPath, " —— successful\n";
-                        break;
-                    case 1:
-                        echo $listPath, " —— failure\n";
-                        break;
-                    case 2:
-                        echo $listPath, " —— exists\n";
-                        break;
-                    default:
-                        echo 'error';
-                        exit;
-                }
-            }
-        }
-    }    
-}
 
-function makeDirectory($path)
-{
-    if (!file_exists($path)) {
-        return mkdir($path, 777) ? 0 : 1;
-        // return mkdir($path, 777) ? $path . ' —— successful' : $path . ' —— failure';
+foreach ($jkxyWikiUrlList as $key => $value) {
+    foreach ($value as $k => $v) {
+        $path = DIRECTORY_PATH . DIRECTORY_SEPARATOR . $key . DIRECTORY_SEPARATOR . $k . DIRECTORY_SEPARATOR;
+        echo makeDirectory($path . 'PDF') && makeDirectory($path . 'ePub') ? $path . ' -- successful' . PHP_EOL : $path . ' -- failure' . PHP_EOL;
     }
-    // return $path . ' —— exists';
-    return 2;
 }
-
