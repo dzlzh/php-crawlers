7e60056200a3258b6036f2d7b65e31cfcbb48fbf
DZLZH <dzlzh@null.net>
2016-07-05 09:56:40 +0200

    function curlHtml

--- function.php
+++ function.php
@@ -7,9 +7,9 @@
  *  +--------------------------------------------------------------
  *  | Filename: function.php
  *  +--------------------------------------------------------------
- *  | Last modified: 2016-07-05 14:58
+ *  | Last modified: 2016-07-05 15:40
  *  +--------------------------------------------------------------
- *  | Description: makeDirectory
+ *  | Description: makeDirectory curlHtml
  *  +--------------------------------------------------------------
  */
 
@@ -30,3 +30,43 @@ function makeDirectory($path, $mode = 777)
     return is_dir(dirname($path)) || makeDirectory(dirname($path)) ? mkdir($path, $mode) : false;
 }
 
+
+/**
+ *  页面采集
+ *  @param string $url
+ *  @param string $userAgent
+ *  @param string $cookie
+ *  @param array  $param
+ *  @return string
+ */ 
+
+function curlHtml($url, $userAgent = null, $cookie = null, $param = null)
+{
+    for ($i = 0; $i < 3; $i++) {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_HEADER, 0);
+        if ($userAgent != null) {
+            curl_setopt($curl, CURLOPT_USERAGENT, $userAgent);
+        }
+        if ($path != null) {
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $param);
+        }
+        if ($cookie != null) {
+            curl_setopt($curl, CURLOPT_COOKIE, $cookie);
+        }
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        $data = curl_exec($curl);
+        $info = curl_getinfo($curl);
+        if ($info['http_code'] == 200) {
+            return $data;
+        }
+        usleep(1000000 * $i);
+    }
+    $error = 'Error::http_code:' . $info['http_code'] . ';url:'. $info['url'];
+    if (is_array($param)) {
+        $error .= ';param:' . '(' . serialize($param) . ')';
+    }
+    return $error;
+}
