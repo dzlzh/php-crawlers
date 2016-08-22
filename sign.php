c554bb07dff7770de527ab6c8be2eba4f3c7300b
DZLZH <dzlzh@null.net>
2016-08-22 13:58:31 +0200

    test startss

--- sign.php
+++ sign.php
@@ -20,6 +20,24 @@ echo "---", DATE, "---\n";
 //引入配置文件
 require_once 'config.php';
 
+//StartSS sign
+if (!empty($URL['StartSS'])) {
+    $StartSSSignURL = $URL['StartSS'];
+    $StartSSSign = curlHtml($StartSSSignURL, null, $COOKIE['StartSS'], $userAgent);
+    print_r($StartSSSign);
+    die;
+    if (strstr($v2dnSign, '302') !== false) {
+        $v2dnURL = $URL['StartSS'] . 'mypoints.php';
+        $v2dnHtml = curlHtml($v2dnURL, null, $COOKIE['StartSS'], $userAgent);
+        $isMatched = preg_match('/((((1[6-9]|[2-9]\d)\d{2})-(1[02]|0?[13578])-([12]\d|3[01]|0?[1-9]))|(((1[6-9]|[2-9]\d)\d{2})-(1[012]|0?[13456789])-([12]\d|30|0?[1-9]))|(((1[6-9]|[2-9]\d)\d{2})-0?2-(1\d|2[0-8]|0?[1-9]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-))/', $v2dnHtml, $matches);
+        if ($isMatched && $matches[0] == substr(DATE, 0, 10)) {
+            echo "v2dn签到成功\n";
+        } else {
+            echo "v2dn签到失败\n";
+        }
+    }
+}
+
 //v2dn sign
 if (!empty($URL['v2dn'])) {
     $v2dnSignURL = $URL['v2dn'] . 'checkIn.php';
