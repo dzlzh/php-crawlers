2d0e41172cabd46dfa589b3a363bdd4b0e55ab4c
DZLZH <dzlzh@null.net>
2016-08-29 08:02:40 +0200

    log output

--- index.php
+++ index.php
@@ -13,6 +13,15 @@
  *  +--------------------------------------------------------------
  */
 
-
-$automaticSignLog = file_get_contents('/root/automatic-sign/sign.log', FILE_USE_INCLUDE_PATH);
+header('Content-Type: text/html; charset=utf-8');
+$automaticSignLog = file_get_contents('automaticSign.log', FILE_USE_INCLUDE_PATH);
+$lagouCrawlerLog  = file_get_contents('lagouCrawler.log', FILE_USE_INCLUDE_PATH);
+echo '<pre>';
+echo 'automatic-sign';
+echo "\n";
 echo $automaticSignLog;
+echo "\n\n";
+echo 'lagou-crawler';
+echo "\n";
+echo $lagouCrawlerLog;
+echo '</pre>';
