d7b44fa928a8686973da4f4efe67349d376506d1
DZLZH <dzlzh@null.net>
2017-10-16 15:38:25 +0800

    v2ex签到

--- index.php
+++ index.php
@@ -17,18 +17,20 @@
 date_default_timezone_set('Asia/Shanghai');
 define('DATE', date('Y-m-d H:i:s'));
 echo "---", DATE, "---\n";
-//引入配置文件
-require_once 'config.php';
+
+$userAgent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36';
+$cookie = '';
+$url    = 'https://www.v2ex.com/mission/daily/';
 
 //v2ex sign
-if (!empty($COOKIE['v2ex'])) {
-    $v2exHtml = curlHtml($URL['v2ex'], null, $COOKIE['v2ex'], $userAgent);
+if (!empty($cookie)) {
+    $v2exHtml = curlHtml($url, null, $cookie, $userAgent);
     $isMatched = preg_match('/\/mission\/daily\/(redeem\?once=\w*)/', $v2exHtml, $matches);
     if ($isMatched) {
-        $v2exSignURL = $URL['v2ex'] . $matches[1];
-        $v2exSign = curlHtml($v2exSignURL, null, $COOKIE['v2ex'], $userAgent);
+        $v2exSignURL = $url . $matches[1];
+        $v2exSign = curlHtml($v2exSignURL, null, $cookie, $userAgent);
         if (strstr($v2exSign, '302') !== false) {
-            $v2exHtml = curlHtml($URL['v2ex'], null, $COOKIE['v2ex'], $userAgent);
+            $v2exHtml = curlHtml($url, null, $cookie, $userAgent);
             $isMatched = preg_match('/\x{6bcf}\x{65e5}\x{767b}\x{5f55}\x{5956}\x{52b1}\x{5df2}\x{9886}\x{53d6}/u', $v2exHtml);
             if ($isMatched) {
                 echo "v2ex签到成功\n";
@@ -46,47 +48,6 @@ if (!empty($COOKIE['v2ex'])) {
             echo "v2ex 签到失败\n";
         }
     }
-}
-
-//v2dn sign
-if (!empty($COOKIE['v2dn'])) {
-    $cookie = getCookie('https://www.v2dn.net/loginUpdate.php', $COOKIE['v2dn'], 'POST');
-    $v2dnSignURL = $URL['v2dn'] . 'checkIn.php';
-    $v2dnSign = curlHtml($v2dnSignURL, null, $cookie, $userAgent);
-    if (strstr($v2dnSign, '302') !== false) {
-        $v2dnURL = $URL['v2dn'] . 'mypoints.php';
-        $v2dnHtml = curlHtml($v2dnURL, null, $cookie, $userAgent);
-        $isMatched = preg_match('/((((1[6-9]|[2-9]\d)\d{2})-(1[02]|0?[13578])-([12]\d|3[01]|0?[1-9]))|(((1[6-9]|[2-9]\d)\d{2})-(1[012]|0?[13456789])-([12]\d|30|0?[1-9]))|(((1[6-9]|[2-9]\d)\d{2})-0?2-(1\d|2[0-8]|0?[1-9]))|(((1[6-9]|[2-9]\d)(0[48]|[2468][048]|[13579][26])|((16|[2468][048]|[3579][26])00))-0?2-29-))/', $v2dnHtml, $matches);
-        if ($isMatched && $matches[0] == substr(DATE, 0, 10)) {
-            $isMatched = preg_match('/<strong>(\d+)/', $v2dnHtml, $matches);
-            if ($isMatched) {
-                echo "v2dn 每日登录奖励$matches[1]\n";
-            } else {
-                echo "v2dn 签到成功\n";
-            }
-        } else {
-            echo "v2dn 签到失败\n";
-        }
-    }
-}
-
-//StartSS sign
-if (!empty($COOKIE['StartSS'])) {
-    $StartSSSignURL = $URL['StartSS'];
-    $cookie = getCookie('https://startss.today/s?wd=%E5%B9%BD%E8%B0%B7%E6%B8%85%E6%B3%89');
-    $cookie .= getCookie('https://startss.today/auth/login', $COOKIE['StartSS'], 'POST', $cookie); 
-    $StartSSSign = curlHtml($StartSSSignURL, '1', $cookie, $userAgent);
-    $StartSSSign = json_decode($StartSSSign, true);
-    echo 'StartSS:', $StartSSSign['msg'], "\n";
-}
-
-//ssfastproxy sign
-if (!empty($COOKIE['ssfastproxy'])) {
-    $ssfastproxySignURL = $URL['ssfastproxy'];
-    $cookie = getCookie('https://ssfastproxy.com/auth/login', $COOKIE['ssfastproxy'], 'POST');
-    $ssfastproxySign = curlHtml($ssfastproxySignURL, '1', $cookie, $userAgent);
-    $ssfastproxySign = json_decode($ssfastproxySign, true);
-    echo 'ssfastproxy:', $ssfastproxySign['msg'], "\n";
 }
 
 function curlHtml($url, $param = null, $cookie = null, $userAgent = null)
